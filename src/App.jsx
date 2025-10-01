df48bf462ede46246adf4081d78ea53e72bf37ec
BgYato <andresfyatem@gmail.com>
2025-10-01 17:41:56 -0500

    Finish other sections and adjust view on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,19 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ScrollButtons from "./components/ScrollButtons";
 import CV from "./components/CV";
+import { use, useState } from "react";
 function App() {
+  const [isClickable, setIsClickable] = useState(true);
+
   return (
     <div className="bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white">
-      {" "}
-      <Navbar /> <Hero /> <About /> <Skills /> <Projects /> <Timeline /> <CV />{" "}
-      <ScrollButtons />
-      <Contact /> <Footer />{" "}
+      <Navbar /> <Hero /> <About /> <Skills /> <Projects />
+      <Timeline
+        isClickable={isClickable}
+        setIsClickable={setIsClickable}
+      />
+      <CV /> <ScrollButtons isClickable={isClickable} />
+      <Contact /> <Footer />
     </div>
   );
 }
--- src/utils/eventsData.js
+++ src/utils/eventsData.js
@@ -1,49 +1,67 @@
 export const events = [
   {
-    year: "2019",
-    description: "Mi primer acercamiento serio al mundo de la programación con Java.",
+    year: "2021 - 2022",
+    description: "Mi primer acercamiento al mundo de la programación.",
     highlights: [
-      "Aprendí lo básico de programación orientada a objetos.",
+      "Aprendí sobre el desarrollo en PHP nativo.",
       "Resolví ejercicios de estructuras de datos y algoritmos.",
-    ],
-    images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400"
-    ]
-  },
-  {
-    year: "2022",
-    description: "Finalicé mi formación tecnológica en el SENA, fortaleciendo mis bases en desarrollo de software.",
-    highlights: [
+      "Me familiaricé con bases de datos MySQL.",
+      "Desarrollé el proyecto OTAVO web.",
       "Graduado como Tecnólogo en Análisis y Desarrollo de Software.",
-      "Participación en proyectos académicos de software empresarial."
     ],
     images: [
-      "/images/sena-graduacion.jpg"
+      "https://imgur.com/oFW3U7R.png",
+      "https://imgur.com/fmT8I8T.png"
     ]
   },
   {
     year: "2023",
+    description: "Profundización en analisis y desarrollo de software.",
+    highlights: [
+      "Participación en el proyecto OPT a nivel académico.",
+      "Desarrollo del proyecto Nano Navigators para SENASoft 2023.",
+      "Participación en eventos de programación (WordSkills 2023 y SENASoft 2023).",
+      "Aprendizaje de más tecnologías como Java, Spring Boot, React, Vue, etc.",
+      "Consigo contrato de aprendizaje en Globant.",
+    ],
+    images: [
+      "https://imgur.com/FrWrJZy.png",
+      "https://imgur.com/tsrNiOW.png",
+      "https://imgur.com/qi1bmi4.png" 
+    ]
+  },
+  {
+    year: "2024",
     description: "Tuve la oportunidad de trabajar en Globant durante un año, aprendiendo del mundo real de IT.",
     highlights: [
       "Colaboré en proyectos ágiles con equipos multidisciplinarios.",
       "Mejoré habilidades en backend y frontend.",
-      "Uso de metodologías ágiles y herramientas DevOps."
+      "Uso de metodologías ágiles y herramientas DevOps.",
+      "Participé en ParkingME y Glober Market",
+      "Participé en eventos internos de Globant.",
+      "Realicé cursos y certificaciones en Globant University y Udemy .",
+      "Graduado como Técnico Profesional en Análisis y Desarrollo de Software.",
     ],
     images: [
-      "/images/globant1.jpg",
-      "/images/globant2.jpg"
+      "https://imgur.com/7eA5ofT.png",
+      "https://imgur.com/41kuV0y.png",
+      "https://imgur.com/ncHTBqi.png",
+      "https://imgur.com/BDiJrc2.png",
+      "https://imgur.com/NZG4phB.png",
+      "https://imgur.com/OhyhI7v.png"
     ]
   },
   {
-    year: "2025",
+    year: "2025 - Presente",
     description: "Actualmente curso Ingeniería de Sistemas en la CUN para fortalecer mi perfil profesional.",
     highlights: [
       "Aprendizaje de nuevas áreas como BI y arquitecturas empresariales.",
-      "Participación en proyectos universitarios y personales."
+      "Participación en proyectos universitarios y personales.",
+      "Desarrollo continuo de habilidades técnicas y blandas.",
+      "Exploración de nuevas tecnologías y tendencias en IT.",
     ],
     images: [
-      "/images/cun1.jpg"
+      null
     ]
   }
 ];
